0342723f98150057c34fbb67f96f7e20e0dd5529
stani4 <37027670+stani4@users.noreply.github.com>
2024-02-25 23:11:29 +0200

    Initial commit

---
+++ src/main/java/Intro/MyFirstClass.java
@@ -0,0 +1,12 @@
+package Intro;
+
+public class MyFirstClass {
+    //static method
+    public static void printHello(){
+        System.out.println("Hello");
+    }
+//Main method
+public static void main(String[] args) {
+    printHello();
+    }
+}
